5e756748eddba64c436d4fdaa3a0d7e1c1af85a5
paulotarciso <paulotarciso@meta.edu.br>
2017-10-02 02:34:55 -0300

    Primeiro commit

---
+++ menu-efeitos/imgmenu.php
@@ -0,0 +1,12 @@
+		<div class="jimgMenu row">
+		  	<ul class="col-xs-12">
+				<li id="landscapes"><a href="#nogo">Flores</a></li>
+				<li id="people"><a href="#nogo">People</a></li>
+				<li id="nature"><a href="#nogo">Nature</a></li>
+				<li id="abstract"><a href="#nogo">Abstract</a></li>
+				<li id="urban"><a href="#nogo">Urban</a></li>
+			</ul>
+		</div>
+		<br  style="clear:both"/><br />
+
+
